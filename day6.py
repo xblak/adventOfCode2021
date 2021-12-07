1c203ac150cb0150c4dc9f3d53c1595d06138bbc
Blake <xblak@users.noreply.github.com>
2021-12-06 22:23:15 -0800

    day 6 solution

---
+++ day6.py
@@ -0,0 +1,43 @@
+import numpy as np
+def naivesolution():
+    from numpy import loadtxt
+    fishes = loadtxt("test.txt", dtype=int, delimiter=',')
+    days = 80
+    for _ in range(days):
+        fishes -= 1
+        newfish = 0
+        for i in range(len(fishes)):
+            if fishes[i] == -1:
+                fishes[i] = 6
+                newfish += 1
+        fishes = np.append(fishes, [8] * newfish)
+        print(fishes)
+    print(len(fishes))
+
+def anotherday(map):
+    # fishes have 0 day produce new fishes
+    newfish = map[0]
+    # move all fishes to one less day
+    for i in range(len(map)-1):
+        map[i] = map[i+1]
+    map[8] = newfish
+    map[6] += newfish
+    print(map)
+
+def countfish(map):
+    # count total fishes in the map
+    sum = 0
+    for fish in map:
+        sum += fish
+    return sum
+
+if __name__ == '__main__':
+    # naivesolution()
+    with open("input6.txt") as f:
+        fishes = f.readline().strip().split(',')
+        map = [0] * 9
+        for fish in fishes:
+            map[int(fish)] += 1
+        for _ in range(256):
+            anotherday(map)
+        print(countfish(map))
