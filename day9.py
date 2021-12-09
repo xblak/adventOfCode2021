885f75a8e901710f0fd9d7ec1b2a302d007d5c0c
Blake <xblak@users.noreply.github.com>
2021-12-09 11:59:43 -0800

    day 9 solution - more accurate variable names

--- day9.py
+++ day9.py
@@ -1,59 +1,59 @@
 import numpy as np
 
 
-def getSumRishLevel(heightmap):
+def getSumRishLevel(heightMap):
     sum = 0
-    for x in range(len(heightmap)):
-        for y in range(len(heightmap[x])):
-            val = int(heightmap[x][y])
+    for x in range(len(heightMap)):
+        for y in range(len(heightMap[x])):
+            val = int(heightMap[x][y])
             # check if val is lower than all adjacent
-            if (isHigherPoint(val, x + 1, y, heightmap) and
-                    isHigherPoint(val, x - 1, y, heightmap) and
-                    isHigherPoint(val, x, y + 1, heightmap) and
-                    isHigherPoint(val, x, y - 1, heightmap)):
+            if (isHigherPoint(val, x + 1, y, heightMap) and
+                    isHigherPoint(val, x - 1, y, heightMap) and
+                    isHigherPoint(val, x, y + 1, heightMap) and
+                    isHigherPoint(val, x, y - 1, heightMap)):
                 sum += val + 1
     return sum
 
 
-def isNotInBound(x, y, heightmap):
-    return x < 0 or x >= len(heightmap) or y < 0 or y >= len(heightmap[x])
+def isNotInBound(x, y, heightMap):
+    return x < 0 or x >= len(heightMap) or y < 0 or y >= len(heightMap[x])
 
 
-def isHigherPoint(val, x, y, heightmap):
-    return isNotInBound(x, y, heightmap) or val < int(heightmap[x][y])
+def isHigherPoint(val, x, y, heightMap):
+    return isNotInBound(x, y, heightMap) or val < int(heightMap[x][y])
 
 
-def isPartOfBasin(x, y, heightmap, visited) -> int:
+def getBasinSize(x, y, heightMap, visited) -> int:
     # stop if not in bound or visited
-    if isNotInBound(x, y, heightmap) or visited[x][y]:
+    if isNotInBound(x, y, heightMap) or visited[x][y]:
         return 0
     else:
         visited[x][y] = True
         # if < 9 means a connected valid basin
         # return 1 + all four directions, dfs
-        if int(heightmap[x][y]) < 9:
-            return 1 + (isPartOfBasin(x + 1, y, heightmap, visited) +
-                        isPartOfBasin(x - 1, y, heightmap, visited) +
-                        isPartOfBasin(x, y + 1, heightmap, visited) +
-                        isPartOfBasin(x, y - 1, heightmap, visited))
+        if int(heightMap[x][y]) < 9:
+            return 1 + (getBasinSize(x + 1, y, heightMap, visited) +
+                        getBasinSize(x - 1, y, heightMap, visited) +
+                        getBasinSize(x, y + 1, heightMap, visited) +
+                        getBasinSize(x, y - 1, heightMap, visited))
     return 0
 
 
-def getProdBasin(heightmap):
+def getProdBasin(heightMap):
     # list to store all basin size
-    allbasin = []
-    row = len(heightmap)
-    col = len(heightmap[0])
+    allBasin = []
+    row = len(heightMap)
+    col = len(heightMap[0])
     # np zeros is easier
     # visited = [[False] * col for i in range(row)]
     visited = np.zeros((row, col))
     # go over the matrix
     for x in range(row):
         for y in range(col):
-            allbasin.append(isPartOfBasin(x, y, heightmap, visited))
+            allBasin.append(getBasinSize(x, y, heightMap, visited))
     # we only need 3 largest, priority queue or k largest should be better
-    allbasin.sort(reverse=True)
-    return np.prod(allbasin[:3])
+    allBasin.sort(reverse=True)
+    return np.prod(allBasin[:3])
 
 
 if __name__ == '__main__':
